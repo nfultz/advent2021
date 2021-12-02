d2bc2bbc49eacc3caa003528e1b73b13551d8a9b
Neal Fultz <nfultz@ucla.edu>
2021-12-02 11:02:25 -0800

    day2

---
+++ day2.R
@@ -0,0 +1,8 @@
+df <- read.delim("input/day2", header = FALSE, sep = " ", stringsAsFactors = TRUE)
+with(xtabs(V2~V1, df) |> as.list(), forward * (down - up))
+
+
+df$aim <- cumsum(mapply(switch, as.character(df$V1), down=df$V2, up=-df$V2, forward=0))
+
+horiz <- sum(df[df$V1 == 'forward', "V2"])
+depth <- with(df[df$V1 == 'forward', ], sum(V2 * aim))
