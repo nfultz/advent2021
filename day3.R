12125f677b21f36fc6bab2dd35627a13574677b1
Neal Fultz <nfultz@ucla.edu>
2021-12-03 09:33:17 -0800

    day3

---
+++ day3.R
@@ -0,0 +1,35 @@
+df <- read.delim("input/day3", sep="", header=FALSE, colClasses = "character")
+
+
+bits <- strsplit(df$V1, "") |> do.call(what=rbind)
+
+gamma <- (apply(bits, 2, table) |> apply(2, which.max) - 1)
+epsilon <- 1 - gamma
+
+tonum <- \(x) strtoi(paste(x, collapse=""), base = 2)
+tonum(gamma) * tonum(epsilon)
+
+
+oxygen <- bits
+for(i in seq(ncol(bits))) {
+  
+  tbl <- table(oxygen[,i])
+  
+  j <- if(tbl[1] == tbl[2]) 1 else which.max(tbl) - 1
+  
+  oxygen <- oxygen[ oxygen[,i] == j , ]
+}
+oxygen
+
+co2 <- bits
+for(i in seq(ncol(bits))) {
+  
+  tbl <- table(co2[,i])
+  
+  j <- if(tbl[1] == tbl[2]) 0 else which.min(tbl) - 1
+  
+  co2 <- co2[ co2[,i] == j , ]
+}
+co2
+
+tonum(oxygen) * tonum(co2)
